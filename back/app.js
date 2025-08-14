bcc2b0a1e2441464cb9034409bb3bda3a0ae940e
Yvonne Lam <yvonne-ung@hotmail.com>
2025-08-14 18:07:08 +0200

    feat(2): add mkdocs config + swagger

--- back/app.js
+++ back/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/user.route');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  methods: ['GET','POST','PUT','DELETE'],
-  credentials: true
-}));
-
 app.use(express.json());
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mini POC API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const userRoutes = require('./routes/user.route');
 app.use('/api/users', userRoutes);
 
 module.exports = app;
---
+++ docs/docs/index.md
@@ -0,0 +1,7 @@
+# Bienvenue dans la documentation Mini POC
+
+Ce site centralise la documentation de votre projet.
+
+- Frontend : http://localhost:3000
+- Backend : http://localhost:5000
+- API Swagger : http://localhost:5001/api-docs
